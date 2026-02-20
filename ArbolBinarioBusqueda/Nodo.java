751a4f0a832ebcdfa246775fc5e40acfe7177239
Ilse Marianne Castillo Pimienta <L23170117@culiacan.tecnm.mx>
2026-02-19 23:32:11 -0700

    Create Nodo class for binary search tree

---
+++ ArbolBinarioBusqueda/Nodo.java
@@ -0,0 +1,12 @@
+public class Nodo {
+    
+    String nombre;
+    Nodo izq;
+    Nodo der;
+
+    public Nodo(String nombre){
+        this.nombre = nombre;
+        this.izq = null;
+        this.der = null;
+    }
+}
