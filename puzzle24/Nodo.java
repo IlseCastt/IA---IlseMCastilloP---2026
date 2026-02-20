2c6b8c725b522d7b6cf368244a1fa79a5d651a50
Ilse Marianne Castillo Pimienta <L23170117@culiacan.tecnm.mx>
2026-02-19 23:30:39 -0700

    Add Nodo class for puzzle state representation

---
+++ puzzle24/Nodo.java
@@ -0,0 +1,30 @@
+package puzzle24;
+
+public class Nodo implements Comparable<Nodo> {
+    public String estado;
+    public Nodo padre;
+    public String movimiento;
+    int nivel;
+    int costoF;
+
+    public Nodo(String estado, Nodo padre, String movimiento, int nivel) {
+        this.estado = estado;
+        this.padre = padre;
+        this.movimiento = movimiento;
+        this.nivel = nivel;
+        this.costoF = 0;
+    }
+
+    public Nodo(String estado, Nodo padre, String movimiento, int nivel, int costoF) {
+        this.estado = estado;
+        this.padre = padre;
+        this.movimiento = movimiento;
+        this.nivel = nivel;
+        this.costoF = costoF;
+    }
+
+    //el menor costoF es el que va primero
+    public int compareTo(Nodo otro) {
+        return Integer.compare(this.costoF, otro.costoF);
+    }
+}
