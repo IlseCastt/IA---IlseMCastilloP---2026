9ed33c8f32128c3578d97f059ee9e39245c7c815
Ilse Marianne Castillo Pimienta <L23170117@culiacan.tecnm.mx>
2026-03-02 21:28:28 -0700

    Update initial state and enhance output messages

--- puzzle24/Main.java
+++ puzzle24/Main.java
@@ -4,12 +4,19 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String estadoI = "123456789ABC EFGHDJKLMINO"; 
+        String estadoI = "12 456379ABD8EFGHCJKLMINO";; 
         String estadoF = "123456789ABCDEFGHIJKLMNO ";
 
         SolucionPuzzle24 solucion = new SolucionPuzzle24(estadoI, estadoF);
 
-        System.out.println("INICIANDO BUSQUEDA EN HEURÍSTICA");
-        solucion.busquedaHeuristica();
+        System.out.println("RESOLVIENDO PUZZLE 24 CON ESTADO INICIAL: " + estadoI);
+
+        //Distancia de Manhattan
+        System.out.println("\nHeurística: Distancia de Manhattan");
+        solucion.busquedaIDA(1); //usamos la heurística de Manhattan
+
+        //Distancia de Manhattan + Conflicto lineal
+        System.out.println("\nHeurística: Distancia de Manhattan + Conflicto lineal");
+        solucion.busquedaIDA(2); //usamos la heurística de Manhattan + conflicto lineal
     }
 }
